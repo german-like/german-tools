f7eb43c176a2cb96537a276d0596594ece376684
german-like <germaaaaaan.sub@gmail.com>
2025-12-27 19:32:17 +0900

    Create mapstyle.js

---
+++ map_gen/mapstyle.js
@@ -0,0 +1,8 @@
+const inputRange = document.getElementById("inputRange");
+const activeColor = "#377494";
+const inactiveColor = "#dddddd";
+
+inputRange.addEventListener("input", function() {
+  const ratio = (this.value - this.min) / (this.max - this.min) * 100;
+  this.style.background = `linear-gradient(90deg, ${activeColor} ${ratio}%, ${inactiveColor} ${ratio}%)`;
+});
